aa6408e06c46812ed5c9394b4f351f156d147e59
Leah Sherrell and Maggie Harrington <maggie@>
2017-03-01 16:52:15 -0800

    work on routes

---
+++ app/app.php
@@ -0,0 +1,41 @@
+<?php
+
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Book.php";
+    require_once __DIR__."/../src/Author.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:8889;dbname=library';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app['debug'] = true;
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/test", function() use ($app){
+        return $app['twig']->render('test.html.twig');
+    });
+
+    // $app->get("/test", function() use ($app){
+    //     return $app['twig']->render('test.html.twig');
+    // });
+
+    $app->get("/librarian", function() use ($app) {
+        return $app['twig']->render('librarian.html.twig', array('books' => Book::getAll(), 'authors' => Author::getAll()));;
+    });
+
+
+    return $app;
+ ?>
